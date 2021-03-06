4917742ad41976363f897f8adeff0abedc4c5754
Nora Martin <noramartiin96@gmail.com>
2021-03-06 11:50:49 +0100

    prueba

--- routes/auth.user.routes.js
+++ routes/auth.user.routes.js
@@ -1,17 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const UserModel = require('../models/User.model');
+const bcrypt = require("bcryptjs");
+const UserModel = require("../models/User.model");
 
 // will handle POST requests of signIN/signUp to http://localhost:5005/api/user/log
-router.post('/user/log', (req, res) => {
+router.post("/user/log", (req, res) => {
   const { email, password } = req.body;
 
   // -----SERVER SIDE VALIDATION ----------
 
   if (!email || !password) {
     res.status(500).json({
-      errorMessage: 'Please enter username, email and password',
+      errorMessage: "Please enter username, email and password",
     });
     return;
   }
@@ -36,16 +36,16 @@ router.post('/user/log', (req, res) => {
   // }
 
   UserModel.findOne({ email })
-    .then(response => {
+    .then((response) => {
       if (response) {
         //if there is a user, loggedIN
         bcrypt
           .compare(password, response.password)
-          .then(doesItMatch => {
+          .then((doesItMatch) => {
             //if it matches
             if (doesItMatch) {
               // req.session is the special object that is available to you
-              response.password = '***';
+              response.password = "***";
               req.session.loggedInUser = response;
               res.status(200).json(response);
             }
@@ -59,7 +59,7 @@ router.post('/user/log', (req, res) => {
           })
           .catch(() => {
             res.status(500).json({
-              error: 'Email format not correct',
+              error: "Email format not correct",
             });
             return;
           });
@@ -70,21 +70,21 @@ router.post('/user/log', (req, res) => {
         let salt = bcrypt.genSaltSync(10);
         let hash = bcrypt.hashSync(password, salt);
         UserModel.create({ email, password: hash })
-          .then(user => {
+          .then((user) => {
             // ensuring that we don't share the hash as well with the user
-            user.passwordHash = '***';
+            user.passwordHash = "***";
             req.session.loggedInUser = response;
             res.status(200).json(user);
           })
-          .catch(err => {
+          .catch((err) => {
             if (err.code === 11000) {
               res.status(500).json({
-                errorMessage: 'username or email entered already exists!',
+                errorMessage: "username or email entered already exists!",
                 message: err,
               });
             } else {
               res.status(500).json({
-                errorMessage: 'Something went wrong! Go to sleep!',
+                errorMessage: "Something went wrong! Go to sleep!",
                 message: err,
               });
             }
@@ -92,7 +92,7 @@ router.post('/user/log', (req, res) => {
       }
     })
     .catch(() => {
-      console.log('Something went wrong logging or creating a user');
+      console.log("Something went wrong logging or creating a user");
     });
 });
 
@@ -103,7 +103,7 @@ const isLoggedIn = (req, res, next) => {
     next();
   } else {
     res.status(401).json({
-      message: 'Unauthorized user',
+      message: "Unauthorized user",
       code: 401,
     });
   }
@@ -111,42 +111,42 @@ const isLoggedIn = (req, res, next) => {
 
 // THIS ARE PROTECTED ROUTE
 // will handle all get requests to http://localhost:5005/api/user
-router.get('/user', isLoggedIn, (req, res, next) => {
+router.get("/user", isLoggedIn, (req, res, next) => {
   res.status(200).json(req.session.loggedInUser);
 });
 // will handle all DELETE requests to http://localhost:5005/api/user
-router.delete('/user', isLoggedIn, (req, res) => {
+router.delete("/user", isLoggedIn, (req, res) => {
   UserModel.findByIdAndDelete(req.session.loggedInUser._id)
-    .then(response => {
+    .then((response) => {
       res.status(200).json(response);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).json({
-        error: 'Something went wrong',
+        error: "Something went wrong",
         message: err,
       });
     });
 });
 
 // will handle all PATCH requests to http://localhost:5005/api/user
-router.patch('/user', isLoggedIn, (req, res) => {
+router.patch("/user", isLoggedIn, (req, res) => {
   let id = req.session.loggedInUser._id;
   const { email, password } = req.body;
   TodoModel.findByIdAndUpdate(id, { email, password })
-    .then(response => {
+    .then((response) => {
       res.status(200).json(response);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json({
-        error: 'Something went wrong',
+        error: "Something went wrong",
         message: err,
       });
     });
 });
 
 // will handle all POST requests to http://localhost:5005/api/logout
-router.post('/logout', isLoggedIn, (req, res) => {
+router.post("/logout", isLoggedIn, (req, res) => {
   req.session.destroy();
   // Nothing to send back to the user
   res.status(204).json({});
